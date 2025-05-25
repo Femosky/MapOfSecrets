c7acf1de234a1e544b1eb35f25d05f7219ad0e4f
Olufemi Ojeyemi <olufemiojeyemi@gmail.com>
2025-05-24 23:56:26 -0400

    Backend API URL updated to custom domain

--- src/constants/apiUrls.ts
+++ src/constants/apiUrls.ts
@@ -1,3 +1,3 @@
 // export const API_URL = 'http://localhost:3001';
-export const API_URL = 'http://ec2-18-119-114-112.us-east-2.compute.amazonaws.com:3001';
+export const API_URL = 'https://api-mapofsecrets.femiojeyemi.com';
 export const fetchNoteByPlaceIdLocationType = 'stateProvince'; // either, "cityTown", "stateProvince", or "country"
